1253e1f855f7a33d896b05a8ee0327b723d873bc
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-16 20:05:34 +0530

    updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
+import { UserService } from './user.service';
 
 class UserCreateRequestBody{
     @ApiProperty()  @IsString()  @MinLength(1) name: string;
@@ -8,37 +9,39 @@ class UserCreateRequestBody{
     @ApiProperty() @IsString() @IsEmail() email: string;
 }
 
+class UserUpdateRequestBody extends PartialType(UserCreateRequestBody){}
+
 @Controller('user')
 export class UserController {
-   
+    constructor(private userService: UserService) {}
     
     @Post('/signup')
    async create(@Body() usercreaterequestbody: UserCreateRequestBody){
-        return `user created`;
+        return await this.userService.create(usercreaterequestbody);
     }
 
     
     @Get('/userid')
     async find(@Param('id') id: string){
-        return`hii `;
+        return await this.userService.findOne(id);
     }
 
 
     @Get('/username')
     async getusername(@Param('username') username:string){
-        return `hii `;
+        return await this.userService.getusername(username);
     }
 
     @Patch('/userid')
-    async updateuserdetails( @Param() userId: string ){
-        return `user details updated`;
+    async updateuserdetails(@Body() userupdaterequestbody :UserUpdateRequestBody,  @Param() userId: string ){
+        return await this.userService.updateuserdetails(userupdaterequestbody, userId);
     }
 
 
      
     @Delete('/userid')
     async remove(@Param('id')id: string){
-    return `user deleted`;
+    return await this.userService.remove(id);
     }
    
 }
