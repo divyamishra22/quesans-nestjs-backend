0c5c42b1ac8287bbb08882e15434596f8a93682a
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-17 01:10:00 +0530

    schema for ans created

---
+++ src/ans/ans.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, {  Types } from "mongoose";
+import { Schema } from "@nestjs/mongoose";
+
+export type AnsDocument = Ans & Document;
+
+@Schema()
+export class Ans {
+
+
+  @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Ques'})
+  ques:  Types.ObjectId;
+
+
+// @Prop({required:true , unique: true})
+//   title: string;
+
+  
+  @Prop({required:true , unique:true })
+  answer: string;
+
+  @Prop({ optional: true})
+  date:  Date;
+}
+export const QuesSchema = SchemaFactory.createForClass(Ans);
