f50980ede8e4e8c2b7747f0f9e700c78e271e9b1
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-16 18:58:07 +0530

    mongo connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuesController } from './ques/ques.controller';
+import { QuesService } from './ques/ques.service';
+import { QuesModule } from './ques/ques.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://divya:divya123@127.0.0.1:27017/mydbqa',
+    ),
+    
+    ,QuesModule],
+  controllers: [AppController, QuesController],
+  providers: [AppService, QuesService],
 })
 export class AppModule {}
