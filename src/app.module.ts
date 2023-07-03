83f336db41d0225336e7a5aaa31d9a12dd4f2300
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-07-03 14:17:15 +0530

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuesController } from './ques/ques.controller';
-import { QuesService } from './ques/ques.service';
 import { QuesModule } from './ques/ques.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
-import { AnsController } from './ans/ans.controller';
-import { AnsService } from './ans/ans.service';
 import { AnsModule } from './ans/ans.module';
 
+
+require('dotenv').config();
 
 @Module({
   imports: [
      MongooseModule.forRoot(
       //  'mongodb://divya:divya123@127.0.0.1:27017/mynewdb1',
-         'mongodb://127.0.0.1:27017/mydbqa',
-      
+        //  'mongodb://127.0.0.1:27017/mydbqa',
+        process.env.MONGODB_URI,
     )
     
       ,QuesModule, UserModule, AuthModule, AnsModule],
